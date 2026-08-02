6131d752869551ea904742de4b12e21d876035c1
Sabit <sabitaljuma@gmail.com>
2026-08-02 06:15:02 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract,multiply
 
 print("Name : Sabit Al Juma")
 Date= date.today()
@@ -9,3 +9,6 @@ Addition = add(10, 5)
 print(f"Add: {Addition}")
 Subtraction = subtract(10, 5)
 print(f"Subtract: {Subtraction}")
+
+Multiplication = multiply(10, 5)
+print(f"Multiply: {Multiplication}")
